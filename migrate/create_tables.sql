2d5a859b03e3f3634417def86f4bec3be17d4aaa
vs06 <victor.minduim@gmail.com>
2014-01-28 09:41:17 -0200

    Create create_tables.sql

---
+++ migrate/create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `habilitadb`.`question` (
+  `id` BIGINT(20) AUTO_INCREMENT,
+  `pergunta` VARCHAR(400) NULL ,
+  `a` VARCHAR(255) NULL ,
+  `b` VARCHAR(255) NULL ,
+  `c` VARCHAR(255) NULL ,
+  `d` VARCHAR(255) NULL ,
+  `e` VARCHAR(255) NULL ,
+  `resposta` VARCHAR(20) NULL ,
+PRIMARY KEY(id) );
